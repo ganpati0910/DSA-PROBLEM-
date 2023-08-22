3f08ee5c805500bb52e77cb2812c354ef7956c73
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2023-08-22 11:07:54 +0530

    Time: 24 ms (78.12%) | Memory: 6.4 MB (31.12%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public: 
+     bool issafe(int row,int col,vector<vector<char>>&board,char val){
+         for(int i=0; i<board.size(); i++){
+             if(board[row][i]==val){
+                 return false;
+             }
+             if(board[i][col]==val){
+                 return false;
+             }
+             if(board[3*(row/3)+i/3][3*(col/3) +i%3]==val){
+                 return false;
+             }
+         }
+         return true;
+     }
+      bool solve(vector<vector<char>>&board){
+          int n=board[0].size();
+         for(int i=0; i<n; i++){
+             for(int j=0; j<n; j++){
+                 if(board[i][j]=='.'){
+                     for(char val='1'; val<='9'; val++){
+                         if(issafe(i,j,board,val)){
+                             board[i][j]=val;
+                             if(solve(board)){
+                                 return true;
+                             }
+                              else{
+                              board[i][j]='.';
+                               }
+                         }
+                     }
+                     return false;
+                 }
+             }
+         }
+         return true; 
+      }
+    void solveSudoku(vector<vector<char>>& board) {
+       solve(board);
+    }
+};
