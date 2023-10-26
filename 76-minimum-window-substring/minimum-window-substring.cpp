37f931d96e0c29645be62fbe700d8205c4171a45
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2023-10-27 00:11:33 +0530

    Time: 17 ms (57.34%) | Memory: 8.2 MB (51.83%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -6,7 +6,7 @@ public:
             mp[t[i]]++;
         }
         int mini=INT_MAX;
-        int count=mp.size();
+        int count=0;
         int i=0;
         int j=0;
         int start=-1;
@@ -14,11 +14,11 @@ public:
             if(mp.find(s[j])!=mp.end()){
                 mp[s[j]]--;
                 if(mp[s[j]]==0){
-                    count--;
+                    count++;
                 }
             } 
-            if(count==0){
-                 while(i<=j&&count==0){
+            if(count==mp.size()){
+                 while(i<=j&&count==mp.size()){
                     if(mini>j-i+1){
                         mini=j-i+1;
                         start=i;
@@ -26,7 +26,7 @@ public:
                     if(mp.find(s[i])!=mp.end()){
                         mp[s[i]]++;
                         if(mp[s[i]]==1){
-                            count++;
+                            count--;
                         }  
                     }
                     i++;
