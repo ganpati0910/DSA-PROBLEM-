7efa78f538c95b9da24f22ab1843edeb9c3a6b06
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2023-10-31 00:49:17 +0530

    Time: 4 ms (58.15%) | Memory: 22.2 MB (15.49%) - LeetSync

--- 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.cpp
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    int minNumber(vector<int>& v1, vector<int>& v2) {
-      vector<int>v(10,0);
-      for(int i=0; i<v1.size(); i++){
-          v[v1[i]]++;
-      }
-      for(int i=0; i<v2.size(); i++){
-          v[v2[i]]++;
-      }
-      for(int i=1; i<=9; i++){
-          if(v[i]>1){
-              return i;
-          }
-      }
-       sort(v1.begin(),v1.end());
-       sort(v2.begin(),v2.end());
-       return min((v1[0]+v2[0]*10),(v1[0]*10+v2[0]));
-
-
-    // sort(nums1.begin(),nums1.end());
-    // sort(nums2.begin(),nums2.end());
-
-    // for(int i=0; i<nums1.size(); i++){
-    //     for(int j=0; j<nums2.size(); j++){
-    //         if(nums1[i]==nums2[j]){
-    //             return nums1[i];
-    //         }
-    //     }
-    // }
-    //     return min((nums1[0]+nums2[0]*10),(nums1[0]*10+nums2[0]));
+    int minNumber(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>v(10,0);
+        for(int i=0; i<nums1.size(); i++){
+            v[nums1[i]]++;
+        }
+        for(int i=0; i<nums2.size(); i++){
+            v[nums2[i]]++;
+        }
+        for(int i=1; i<=9; i++){
+            if(v[i]>1){
+                return i;
+            }
+        }
+     sort(nums1.begin(),nums1.end());
+     sort(nums2.begin(),nums2.end());
+     return min((nums1[0]+nums2[0]*10),(nums1[0]*10+nums2[0]));
+        
         
     }
 };
