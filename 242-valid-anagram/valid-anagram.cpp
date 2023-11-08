cff3e2a6b170f3622a001b60466b03f53ff2608a
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2023-11-08 18:57:39 +0530

    Time: 7 ms (76.95%) | Memory: 8 MB (8.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,19 +4,18 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        unordered_map<char,int>mp1;
-        unordered_map<char,int>mp2;
-
-        for(int i=0; i<s.size(); i++){
-            mp1[s[i]]++;
-            mp1[t[i]]--;
-        }
        
-      for(int i=0; i<mp1.size(); i++){
-           if(mp1[i]!=0){
-               return false;
-           }
-       }
-        return true;
+        unordered_map<char,int>mp2;
+        for(int i=0; i<s.size(); i++){
+              mp2[s[i]]++;
+              mp2[t[i]]--;
+        }
+         for(int i=0; i<mp2.size(); i++){
+             if(mp2[i]!=0){
+                 return false;
+             }
+         }
+         return true;
+        
     }
 };
