63bd1964214aa49f4df72be5a0a741ce4b52b510
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2023-11-08 19:06:10 +0530

    Time: 13 ms (35.83%) | Memory: 8.8 MB (75.33%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        int n=s.size();
-        unordered_map<char,int>mp;
-        for(int i=0; i<n; i++){
-            mp[s[i]]++;
-        }
-        priority_queue<pair<int,char>> pq;
-        for(auto i:mp){
-            pq.push({i.second,i.first});
-        }
         string ans="";
+        unordered_map<char,int>mp;
+        for(int i=0; i<s.size(); i++){
+                mp[s[i]]++;
+        }
+         priority_queue<pair<int,char>> pq;
+        for(auto i:mp){
+          pq.push({i.second,i.first});
+        }
         while(!pq.empty()){
-           for(int j=0; j<pq.top().first; j++){
-               ans+=pq.top().second;
-           }
+            for(int j=0; j<pq.top().first; j++){
+                ans+=pq.top().second;
+            }
             pq.pop();
         }
-      return ans;
-        
+        return ans;
     }
 };
