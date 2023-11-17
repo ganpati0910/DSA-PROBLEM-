c9185e26a752c798f24178fea0b38f9d85f07a17
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2023-11-17 23:01:53 +0530

    Time: 196 ms (26.78%) | Memory: 96.5 MB (61.58%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int minPairSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int start=0;
-        int end=nums.size()-1;
-        int maxisum=0;
-        while(start<end){
-            if(nums[start]+nums[end]>maxisum){
-                maxisum=nums[start]+nums[end];
-            }
-            start++;
-            end--;
-        }
-       return maxisum;  
+       sort(nums.begin(),nums.end());
+      int start=0;
+      int end=nums.size()-1;
+      int maxisum=0;
+      while(start<end){
+          if(nums[start]+nums[end]>maxisum){
+              maxisum=nums[start]+nums[end];
+          }
+          start++;
+          end--;
+      }
+      return maxisum;
+
     } 
 };
