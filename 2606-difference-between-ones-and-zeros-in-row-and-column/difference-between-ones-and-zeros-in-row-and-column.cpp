25286cb986812cb013711f019c0ea624d2cce6b3
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2023-12-18 19:30:19 +0530

    Time: 187 ms (72.19%) | Memory: 117.4 MB (59.21%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -3,29 +3,30 @@ public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int onerow,onecol,zerorow,zerocol;
-       vector<vector<int>>ans(n,vector<int>(m,0));
-       vector<int>rowone(n,0);
-       vector<int>colone(m,0);
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        int onesrow,zerorow,onecol,zerocol;
+        vector<int>rowones(n,0);
+        vector<int>colones(m,0);
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j]==1){
-                    rowone[i]++;
-                    colone[j]++;
+                    rowones[i]++;
+                    colones[j]++;
                 }
             }
         }
-
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                onerow=rowone[i];
-                onecol=colone[j];
-                zerorow=n-rowone[i];
-                zerocol=m-colone[j];
-                ans[i][j]=onerow+onecol-zerorow-zerocol;
+                onesrow=rowones[i];
+                zerorow=n-rowones[i];
+                onecol=colones[j];
+                zerocol=m-colones[j];
+                ans[i][j]=onesrow+onecol-zerorow-zerocol;
             }
         }
 
-        return ans;
+      return ans;
+
+        
     }
 };
