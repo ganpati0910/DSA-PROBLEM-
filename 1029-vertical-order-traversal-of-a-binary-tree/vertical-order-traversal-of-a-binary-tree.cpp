4fa9f2152185bb10e1c912ddc5ca1ad912dd3c97
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2023-12-23 19:50:03 +0530

    Time: 4 ms (54.48%) | Memory: 13.9 MB (62.80%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,35 +12,40 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL){
+            return ans;
+        }
         queue<pair<TreeNode*,pair<int,int>>>q;
         map<int,map<int,multiset<int>>>mp;
         q.push({root,{0,0}});
         while(!q.empty()){
             int size=q.size();
-            for(int i=0; i<size; i++){
-                TreeNode*node=q.front().first;
-                int hd=q.front().second.first;
-                int level=q.front().second.second;
-                q.pop();
-                mp[hd][level].insert(node->val);
-                if(node->left!=NULL){
-                    q.push({node->left,{hd-1,level+1}});
-                }
-                if(node->right!=NULL){
-                    q.push({node->right,{hd+1,level+1}});
-                }
+            for(int i=0; i<size;i++){
+            TreeNode*node=q.front().first;
+            int hd=q.front().second.first;
+            int level=q.front().second.second;
+            q.pop();
+            mp[hd][level].insert(node->val);
+            if(node->left!=NULL){
+                q.push({node->left,{hd-1,level+1}});
+            }
+            if(node->right!=NULL){
+                q.push({node->right,{hd+1,level+1}});
+            }
+
             }
         }
-    vector<vector<int>>ans;
-    for(auto i:mp){
+        for(auto i:mp){
             vector<int>col;
-        for(auto j:i.second){
-            for(auto k:j.second){
-              col.push_back(k);
+            for(auto j:i.second){
+                for(auto k:j.second){
+                    col.push_back(k);
+                }
             }
+            ans.push_back(col);
         }
-        ans.push_back(col);
-    }
-      return ans;  
+        return ans;
+        
     }
 };
