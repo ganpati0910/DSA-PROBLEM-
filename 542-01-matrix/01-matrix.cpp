c5feb6dfe980c9e6188e1c7c2d0e6576b99a7472
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2023-12-29 09:07:09 +0530

    Time: 199 ms (8.13%) | Memory: 34.8 MB (30.46%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,49 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
-        int n = grid.size(); 
-    int m = grid[0].size(); 
-    vector<vector<int>>vis(n, vector<int>(m, 0));
-    vector<vector<int>>dis(n, vector<int>(m, 0));
-    queue<pair<pair<int, int>, int>>q; 
-    
-    for(int i = 0; i<n ; i++)
-    {
-        for(int j =0; j<m ; j++)
-        {
-            if(grid[i][j] == 0)
-            {
-                vis[i][j] = 1;            
-                q.push({{i, j}, 0});
-            }
-       
-        }
-    }
-    
-    int delrow[] = {-1,0,1,0}; 
-    int delcol[] = {0,-1,0,1}; 
-    
-    while(!q.empty())
-    {
-        int row = q.front().first.first;
-        int col = q.front().first.second;
-        int steps = q.front().second; 
-        q.pop();
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n= mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
+         for(int  i=0; i<n; i++){
+             for(int j=0; j<m; j++){
+                 if(mat[i][j]==0){
+                     vis[i][j]=1;
+                     q.push({{i,j},0});
+                 }
+             }
+         }
+         int steps;
+         int delrow[]={1,0,-1,0};
+         int delcol[]={0,-1,0,1};
+         while(!q.empty()){
+             int row=q.front().first.first;
+             int col=q.front().first.second;
+             int steps=q.front().second;
+             q.pop();
+             ans[row][col]=steps;
+             for(int i=0; i<4; i++){
+                 int nrow=row+delrow[i];
+                 int mcol=col+delcol[i];
+                 if(nrow>=0&&nrow<n&&mcol>=0&&mcol<m&&vis[nrow][mcol]==0&&mat[nrow][mcol]==1){
+                     q.push({{nrow,mcol},steps+1});
+                     vis[nrow][mcol]=1;
+                 }
+             }
+         }
+         return ans;
         
-        dis[row][col] = steps; 
-        
-        for(int i = 0; i<4; i++)
-        {
-            int nrow = row + delrow[i]; 
-            int ncol = col + delcol[i]; 
-            
-            if(nrow>= 0 && nrow<n && ncol >= 0 && ncol <m && vis[nrow][ncol] ==0 && grid[nrow][ncol] == 1)
-            {
-                vis[nrow][ncol] = 1;
-                q.push({{nrow, ncol}, steps+1});
-            }
-        }
-    }
-    return dis;
     }
 };
