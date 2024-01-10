75f976b4dfcf636fc2f11fdfb9ebb3c564cd4a9d
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-01-11 01:03:31 +0530

    Time: 4 ms (11.76%) | Memory: 6.3 MB (75.47%) - LeetSync

--- 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.cpp
+++ 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int findDelayedArrivalTime(int at, int dt) {
-        if(at+dt==24){
-            return 0;
-        }
-       else if(at+dt>24){
-            return at+dt-24;
-        }
-        else{
-            return at+dt;
-        }
+    //     if(at+dt==24){
+    //         return 0;
+    //     }
+    //    else if(at+dt>24){
+    //         return at+dt-24;
+    //     }
+    //     else{
+    //         return at+dt;
+    //     }
+    int t=at+dt;
+    return t%24;
 
     }
 };
