3b5089d7849159e1d0e0a2e69d235ff3f44d8a4b
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-01-21 12:38:26 +0530

    Time: 29 ms (45.58%) | Memory: 12.7 MB (6.51%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -7,21 +7,18 @@ public:
         long long sum2=0;
         for(int i=0; i<n; i++){
            int miniele=arr[i];
+           int maxiele=arr[i];
            for(int j=i+1; j<n; j++){
                miniele=min(miniele,arr[j]);
-               sum1=sum1+miniele;
+               maxiele=max(maxiele,arr[j]);
+               sum1=sum1+(maxiele-miniele);
+              
            }
         }
 
-        for(int i=0; i<n; i++){
-            int maxele=arr[i];
-            for(int j=i+1; j<n; j++){
-                maxele=max(maxele,arr[j]);
-                sum2=sum2+maxele;
-            }
-        }
-        ans=sum2-sum1;
-        return ans;
+       
+       
+        return sum1;
         
     }
 };
