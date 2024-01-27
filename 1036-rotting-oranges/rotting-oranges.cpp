708916eac0cf9a127acf8c7d10e23f2e7cf0488c
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-01-27 20:09:00 +0530

    Time: 10 ms (26.82%) | Memory: 16 MB (7.40%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -6,39 +6,40 @@ public:
         vector<vector<int>>vis(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>>q;
         for(int i=0; i<n; i++){
-            for(int j=0; j<m;j++){
+            for(int j=0; j<m; j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
                     vis[i][j]=1;
                 }
             }
         }
-        int tm=0;
         int delrow[]={1,0,-1,0};
         int delcol[]={0,-1,0,1};
+        int tm=0;
         while(!q.empty()){
             int row=q.front().first.first;
             int col=q.front().first.second;
             int t=q.front().second;
             q.pop();
-            tm=max(t,tm);
+            tm=max(tm,t);
             for(int i=0; i<4; i++){
-                int nrow=row+delrow[i];
-                int mcol=col+delcol[i];
-                if(nrow>=0&&nrow<n&&mcol>=0&&mcol<m&&vis[nrow][mcol]!=1&&grid[nrow][mcol]==1){
-                    q.push({{nrow,mcol},t+1});
-                    vis[nrow][mcol]=1;
-                }
+                  int nrow=row+delrow[i];
+                  int mcol=col+delcol[i];
+                  if(nrow<n&&nrow>=0&&mcol<m&&mcol>=0&&vis[nrow][mcol]!=1&&grid[nrow][mcol]==1){
+                      q.push({{nrow,mcol},t+1});
+                      vis[nrow][mcol]=1;
+                  }
             }
         }
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(vis[i][j]!=1&&grid[i][j]==1){
+                if(vis[i][j]==0&&grid[i][j]==1){
                     return -1;
                 }
             }
         }
         return tm;
+
         
     }
 };
