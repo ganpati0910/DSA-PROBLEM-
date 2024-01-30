c0f2a31c5015ddd697bbb4543d7a90b31066f129
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-01-30 22:28:09 +0530

    Time: 27 ms (69.58%) | Memory: 29.9 MB (5.51%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,10 +4,9 @@ public:
         int n=nums.size();
         int start=0;
         int end=n-1;
-       
         while(start<=end){
-             int mid=(start+end)/2;
-            if(target==nums[mid]){
+            int mid=(start+end)/2;
+            if(nums[mid]==target){
                 return mid;
             }
             else if(nums[mid]<target){
