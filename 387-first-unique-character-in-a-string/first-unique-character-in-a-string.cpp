70672ef0ee5b085a62be5def72df800e37098b7d
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-02-05 13:56:10 +0530

    Time: 33 ms (56.04%) | Memory: 13.1 MB (10.77%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -5,12 +5,13 @@ public:
         for(int i=0; i<s.size(); i++){
             mp[s[i]]++;
         }
-         for(int i=0; i<s.size(); i++){
-             if(mp[s[i]]==1){
-                 return i;
-             }
-         }
-        return -1;
+        for(int i=0; i<s.size(); i++){
+            if(mp[s[i]]==1){
+                return i;
+            }
+        }
+
+                return -1;
         
     }
 };
