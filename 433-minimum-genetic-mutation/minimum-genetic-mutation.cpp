64a1251275780e8fa40b261ebe4743958ef3cbbf
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-02-06 21:29:48 +0530

    Time: 3 ms (41.34%) | Memory: 8.1 MB (6.72%) - LeetSync

---
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    string s="AGCT";
+    int minMutation(string start, string end, vector<string>& bank) {
+        unordered_set<string>st;
+        for(auto it:bank){
+            st.insert(it);
+        }
+        queue<pair<string,int>>q;
+        q.push({start,0});
+        while(!q.empty()){
+            string node=q.front().first;
+            int move=q.front().second;
+            q.pop();
+            if(node==end){
+                return move;
+            }
+            for(int i=0; i<node.size(); i++){
+                string t=node;
+                for(int j=0; j<4; j++){
+                     string t2=t;
+                     t2[i]=s[j];
+                     if(st.find(t2)!=st.end()){
+                         q.push({t2,move+1});
+                         st.erase(t2);
+                     }
+                }
+            }
+
+
+        }
+            return -1;
+
+    }
+};
