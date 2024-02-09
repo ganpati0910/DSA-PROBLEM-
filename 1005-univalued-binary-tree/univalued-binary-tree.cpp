35f01345ea328625d359bd582d22b4e85ec3450e
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-02-09 17:30:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (10.31%) - LeetSync

--- 1005-univalued-binary-tree/univalued-binary-tree.cpp
+++ 1005-univalued-binary-tree/univalued-binary-tree.cpp
@@ -11,32 +11,26 @@
  */
 class Solution {
 public:
+    void traversal(TreeNode*root,vector<int>&ans){
+        if(root==NULL){
+            return;
+        }
+        traversal(root->left,ans);
+        ans.push_back(root->val);
+        traversal(root->right,ans);
+
+    }
     bool isUnivalTree(TreeNode* root) {
         vector<int>ans;
+        traversal(root,ans);
         set<int>st;
-        if(root==NULL){
-            return true;
-        }
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-           TreeNode* node=q.front();
-           q.pop();
-           ans.push_back(node->val);
-           if(node->left!=NULL){
-               q.push(node->left);
-           }
-           if(node->right!=NULL){
-               q.push(node->right);
-           }
-        }
         for(int i=0; i<ans.size(); i++){
             st.insert(ans[i]);
         }
+        
         if(st.size()!=1){
             return false;
         }
         return true;
-        
     }
 };
