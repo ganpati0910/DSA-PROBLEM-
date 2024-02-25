c8caf1b8e15b1f85699876524627378e6f60c6e6
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-02-25 23:51:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (17.14%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    vector<int> frequencySort(vector<int>& nums){
+    vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int,int>mp;
-        priority_queue<pair<int,int>> pq;
-        int n=nums.size();
-        vector<int>ans;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<nums.size(); i++){
             mp[nums[i]]++;
         }
+        priority_queue<pair<int,int>>pq;
         for(auto it:mp){
             pq.push({-it.second,it.first});
         }
+        vector<int>ans;
         while(!pq.empty()){
-            for(int i=0; i<-(pq.top().first); i++){
-                ans.push_back(pq.top().second);
+            for(int i=0;i<-(pq.top().first); i++){
+            ans.push_back(pq.top().second);
             }
             pq.pop();
         }
         // reverse(ans.begin(),ans.end());
         return ans;
+
         
     }
 };
