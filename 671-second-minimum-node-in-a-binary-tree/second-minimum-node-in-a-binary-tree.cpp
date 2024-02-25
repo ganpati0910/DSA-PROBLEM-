220af87dd3f3e75d1e22b7d8d190df943cfe2e4f
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-02-25 17:39:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (9.60%) - LeetSync

--- 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.cpp
+++ 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.cpp
@@ -6,35 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void preorder(TreeNode*root,set<int>&st){
-        if(root==NULL){
+    void preorder(TreeNode* root, set<int>& st) {
+        if (root == NULL) {
             return;
         }
         st.insert(root->val);
-        preorder(root->left,st);
-        preorder(root->right,st);
+        preorder(root->left, st);
+        preorder(root->right, st);
     }
-    int findSecondMinimumValue(TreeNode* root){
-        set<int>st;
-        preorder(root,st);
-        if(st.size()==1){
-            return-1;
+    int findSecondMinimumValue(TreeNode* root) {
+        set<int> st;
+        preorder(root, st);
+        if (st.size() == 1) {
+            return -1;
         }
-        priority_queue<int>pq;
-        for(auto it:st){
+        priority_queue<int> pq;
+        for (auto it : st) {
             pq.push(it);
-            if(pq.size()>2){
+            if (pq.size() > 2) {
                 pq.pop();
             }
             st.erase(it);
         }
         return pq.top();
-
-        
     }
 };
