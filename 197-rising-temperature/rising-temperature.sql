24c9d15e4de2abff95d2646e045d528bdc05f8ba
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-08-26 15:09:07 +0530

    Time: 515 ms (25.84%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
- select w1.id from weather as w1
- inner join weather as w2
-  on datediff(w1.recorddate,w2.recorddate)=1
- and w1.temperature>w2.temperature
+ 
+ select w1.id
+ from weather w1
+ join weather w2 
+ on datediff(w1.recorddate,w2.recorddate)=1
+ and w1.temperature>w2.temperature;
+ 
  
