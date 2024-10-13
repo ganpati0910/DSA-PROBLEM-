cbf11fd1cb9b26f3bbe8011c6c342b7907087738
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2024-10-13 14:42:01 +0530

    Time: 914 ms (99.18%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,24 +1,26 @@
 # Write your MySQL query statement below
-(select  u.name as results from 
+(select u.name as results from 
 users as u 
-inner join 
+join 
 movierating as mr 
 on u.user_id=mr.user_id
 group by u.user_id
-order by count(*) desc,u.name 
-limit 1)
+order by count(*) desc,u.name limit 1)
 
 union all
 
-(select m.title  as results from 
-movies as m 
-inner join 
+(select m.title as results
+from 
+movies as m
+join 
 movierating as mr 
 on m.movie_id=mr.movie_id
 where month(mr.created_at)=2 and year(mr.created_at)=2020
 group by mr.movie_id
-order by avg(mr.rating)
-desc,m.title limit 1)
+order by avg(mr.rating) desc , m.title limit 1)
+
+
+
 
 
 
