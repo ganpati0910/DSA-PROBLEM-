72725851213cab35b6399ec5308af7bb7b0ec5cc
ganpati0910 <139684610+ganpati0910@users.noreply.github.com>
2025-01-18 23:09:09 +0530

    Time: 1351 ms (11.28%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
-select distinct  num as consecutiveNums from
-(select num,
-lead(num,1) over(order by id) as num2,
+with cte as (select 
+num,lead(num,1) over(order by id) as num2,
 lead(num,2) over(order by id) as num3
-from logs) as logs_table
-where num=num2 and num=num3
+from logs)
+
+select distinct num as consecutivenums 
+from cte 
+where num=num2 and num2=num3;
